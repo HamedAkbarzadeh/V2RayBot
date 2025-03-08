42b0262cf1eeb28d0f5f33a7f56a10af8c568923
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-03-08 20:42:02 +0330

    initialize project

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const TelegramBot = require("node-telegram-bot-api")
+const dotenv = require("dotenv")
+
+dotenv.config();
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+
+bot.onText(/\/start/, (msg, match) => {
+    //code
+})
